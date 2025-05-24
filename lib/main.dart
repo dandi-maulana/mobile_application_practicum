529a48bd2e4853ba12256f3c0fb09346b0f6872d
dandi-maulana <nugraha.rahmadan1304@gmail.com>
2025-05-24 15:46:41 +0700

    Practicum 2 in module 4 = AppBar, TabBar, and BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_application_practicum/pages/latihan_appbar_page.dart';
+import 'package:mobile_application_practicum/pages/latihan_BottomNavigationBar.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Latihan Keempat",
-      home: LatihanAppbarPage(),
+      home: new LatihanBottomNavigationBar(),
     ),
   );
 }
